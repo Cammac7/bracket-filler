46e24c7131f7a199db1e85a6ad1c8e6fb06565e5
Cammac7 <cameron@Camerons-MacBook-Pro.local>
2017-03-13 17:00:55 -0400

    imported other libraries

--- bracketGenerator.py
+++ bracketGenerator.py
@@ -1,6 +1,24 @@
+import sys
+import scipy
+import numpy
+import sklearn
 import pandas
 import matplotlib
+
+#libraries
+from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
 class team:
     rank = 0
 
@@ -15,5 +33,6 @@ round1df = pandas.DataFrame(round1data)
 round1odds = round1df.set_index('Seed')
 
 round1odds.plot()
+scatter_matrix(round1odds)
 plt.show()
 
